7f4e5194f0110b0ca69e6913dbfb8af003a2eb2b
Your Name <your-email@example.com>
2025-08-25 14:03:48 +0300

    add debug.py

--- lib/debug.py
+++ lib/debug.py
@@ -7,3 +7,17 @@ from models import Company, Dev
 if __name__ == '__main__':
     engine = create_engine('sqlite:///freebies.db')
     import ipdb; ipdb.set_trace()
+
+
+# Example
+dev1 = Dev(name="Ash")
+comp1 = Company(name="PokéMart", founding_year=1997)
+session.add_all([dev1, comp1])
+session.commit()
+
+comp1.give_freebie(dev1, "Poké Ball", 50, session)
+dev1.received_one("Poké Ball")  # Should return True
+
+
+freebie = session.query(Freebie).first()
+freebie.print_details()
--- lib/models.py
+++ lib/models.py
@@ -10,20 +10,46 @@ metadata = MetaData(naming_convention=convention)
 Base = declarative_base(metadata=metadata)
 
 class Company(Base):
-    __tablename__ = 'companies'
+    __tablename__ = "companies"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    founding_year = Column(Integer)
 
-    id = Column(Integer(), primary_key=True)
-    name = Column(String())
-    founding_year = Column(Integer())
+    freebies = relationship("Freebie", backref="company")
+    devs = relationship("Dev", secondary="freebies", back_populates="companies")
 
-    def __repr__(self):
-        return f'<Company {self.name}>'
+    @classmethod
+    def oldest_company(cls, session):
+        return session.query(cls).order_by(cls.founding_year).first()
+
+    def give_freebie(self, dev, item_name, value, session):
+        freebie = Freebie(item_name=item_name, value=value, dev=dev, company=self)
+        session.add(freebie)
+        session.commit()
 
 class Dev(Base):
-    __tablename__ = 'devs'
+    __tablename__ = "devs"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
 
-    id = Column(Integer(), primary_key=True)
-    name= Column(String())
+    freebies = relationship("Freebie", backref="dev")
+    companies = relationship("Company", secondary="freebies", back_populates="devs")
 
-    def __repr__(self):
-        return f'<Dev {self.name}>'
+    def received_one(self, item_name):
+        return any(f.item_name == item_name for f in self.freebies)
+
+    def give_away(self, other_dev, freebie, session):
+        if freebie in self.freebies:
+            freebie.dev = other_dev
+            session.commit()
+
+class Freebie(Base):
+    __tablename__ = "freebies"
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+    dev_id = Column(Integer, ForeignKey("devs.id"))
+    company_id = Column(Integer, ForeignKey("companies.id"))
+
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
